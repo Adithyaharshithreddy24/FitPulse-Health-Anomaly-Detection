85af9d0e44fe0b4e072aaa01f29c54f5af7ea1d6
ADITHYA's MAC <mymac@MyMacs-MacBook-Air.local>
2025-12-15 20:52:22 +0530

    edit

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -10,17 +10,18 @@ import streamlit as st
 dlac = DataLoadingAndCleaning()
 cm = CorrelationMatrix(dlac)
 vc = VisualizeCorrelation(dlac, cm)
-plr = PlotlyLinearRegression(dlac, 'distance', 'time_elapsed')
+plr = PlotlyLinearRegression(dlac, "distance", "time_elapsed")
 par = PlottingAllRelationships(dlac)
-c = Clustering(dlac, 'time_elapsed', 'total_calories')
+c = Clustering(dlac, "time_elapsed", "total_calories")
 ad = AnomalyDetection(dlac)
 
 page_container = st.empty()
 
+
 def load_page(title: str):
     with page_container.container():
-        st.title(title, text_alignment='justify')
-        if title == 'Data Loading and Cleaning':
+        st.title(title)
+        if title == "Data Loading and Cleaning":
             st.markdown("### Cleaned Data")
             st.dataframe(dlac.data)
             code = """
@@ -35,31 +36,40 @@ def clean_data(self) -> pd.DataFrame:
     
     return self.data
             """
-            st.code(code, language='python')
-        
+            st.code(code, language="python")
+
         elif title == "Correlation Matrix":
             st.pyplot(cm.fig)
 
-        elif title == 'Plotting Correlations and Visualising Highly Correlating Variables':
-            st.markdown("> Plotting our absolute correlations to see which variables with which correlations can we plot.")
+        elif (
+            title
+            == "Plotting Correlations and Visualising Highly Correlating Variables"
+        ):
+            st.markdown(
+                "> Plotting our absolute correlations to see which variables with which correlations can we plot."
+            )
             st.pyplot(vc.corrs_abs_plot)
-            st.markdown("***We see that variables with absolute correlation greater than ```0.2``` is apt for plotting.***")
+            st.markdown(
+                "***We see that variables with absolute correlation greater than ```0.2``` is apt for plotting.***"
+            )
             st.text("\n")
             st.markdown("## Visualizing the highly correlating variables.")
             for fig in vc.highly_corr_vars_figs:
                 st.plotly_chart(fig)
-        
-        elif title == 'Fitting Regression Line (Distance vs Time Elapsed)':
+
+        elif title == "Fitting Regression Line (Distance vs Time Elapsed)":
             st.plotly_chart(plr.plotly_fig)
 
-        elif title == 'Plotting all Relationships (Scatter Matrix)':
+        elif title == "Plotting all Relationships (Scatter Matrix)":
             st.plotly_chart(par.all_relationships_fig)
-        
-        elif title == 'Clustering (Time Elapsed vs Total Calories)':
+
+        elif title == "Clustering (Time Elapsed vs Total Calories)":
             st.plotly_chart(c.xy_cluster_plot)
 
-        elif title == 'Anomaly Detection':
-            st.markdown("### ***Given some data, this detector says if it significantly deviates from the norm.***")
+        elif title == "Anomaly Detection":
+            st.markdown(
+                "### ***Given some data, this detector says if it significantly deviates from the norm.***"
+            )
             heart_rate = st.number_input("Heart Rate")
             time_elapsed = st.number_input("Time Elapsed During Run")
             average_running_cadence = st.number_input("Average Running Cadence")
@@ -68,28 +78,35 @@ def clean_data(self) -> pd.DataFrame:
             submit_data = st.button("Submit Data")
 
             if submit_data:
-                x_star = [heart_rate, time_elapsed, average_running_cadence, total_calories, distance]
+                x_star = [
+                    heart_rate,
+                    time_elapsed,
+                    average_running_cadence,
+                    total_calories,
+                    distance,
+                ]
                 anomaly = ad.flag_anomaly(x_star)
                 if anomaly:
                     st.warning("The given data is an anomaly.")
                 else:
                     st.success("The given data is not an anomaly.")
 
+
 st.sidebar.title("Fit Pulse Data Analysis and Anomaly Detection")
 
 data_cleaning_button = st.sidebar.selectbox(
     "Choose Subtopic",
     [
-        'Data Loading and Cleaning',
-        'Correlation Matrix',
-        'Plotting Correlations and Visualising Highly Correlating Variables',
-        'Fitting Regression Line (Distance vs Time Elapsed)',
-        'Plotting all Relationships (Scatter Matrix)',
-        'Clustering (Time Elapsed vs Total Calories)',
-        'Anomaly Detection',
-    ]
+        "Data Loading and Cleaning",
+        "Correlation Matrix",
+        "Plotting Correlations and Visualising Highly Correlating Variables",
+        "Fitting Regression Line (Distance vs Time Elapsed)",
+        "Plotting all Relationships (Scatter Matrix)",
+        "Clustering (Time Elapsed vs Total Calories)",
+        "Anomaly Detection",
+    ],
 )
 
 if data_cleaning_button:
-    page_container.empty()          # wipe main area only
-    load_page(data_cleaning_button) # load new page into container
+    page_container.empty()  # wipe main area only
+    load_page(data_cleaning_button)  # load new page into container
